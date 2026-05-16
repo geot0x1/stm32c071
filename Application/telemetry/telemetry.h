e11d07d87ea22467b1b4127ddb67b9986f3f5391
George <geot0x1@gmail.com>
2026-05-16 10:52:03 +0300

    refactor(telemetry): simplify telemetry interval management
    Remove the ability to dynamically set the telemetry interval and replace the stored interval variable with a constant default value. Update timestamp types to use `millis_t` for consistency with the rest of the codebase.
    
    - Remove `telemetry_set_interval_ms` function
    - Replace `telemetry.interval_ms` with `TELEMETRY_DEFAULT_INTERVAL_MS`
    - Change `last_send_ms` type from `uint64_t` to `millis_t`
    - Define `TELEMETRY_BUF_SIZE` locally in the C file

--- Application/telemetry/telemetry.h
+++ Application/telemetry/telemetry.h
@@ -7,9 +7,6 @@
 #include <stdint.h>
 
 #define TELEMETRY_DEFAULT_INTERVAL_MS 1000U
-#define TELEMETRY_MIN_INTERVAL_MS 100U
-#define TELEMETRY_MAX_INTERVAL_MS 3600000U
-#define TELEMETRY_BUF_SIZE 96U
 
 void telemetry_init(void);
 void telemetry_task(void);
@@ -18,7 +15,6 @@ void telemetry_send(void);
 void telemetry_reset(void);
 void telemetry_enable(bool enable);
 bool telemetry_is_enabled(void);
-void telemetry_set_interval_ms(uint32_t ms);
 uint32_t telemetry_get_interval_ms(void);
 
 #endif /* TELEMETRY_H */
